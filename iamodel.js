aae0928fe0061d1c136c19882dac9e1351919747
rodrigoruizcampos <87916116+rodrigoruizcampos@users.noreply.github.com>
2023-11-17 09:51:29 -0300

    Create iamodel.js

---
+++ iamodel.js
@@ -0,0 +1,22 @@
+import {
+HandLandmarker,
+FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+const demosSection = document.getElementById("demos");
+let handLandmarker = undefined;
+let runningMode = "IMAGE";
+let enableWebcamButton: HTMLButtonElement;
+let webcamRunning: Boolean = false;
+
+const createHandLandmarker = async () => {
+const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+handLandmarker = await HandLandmarker.createFromOptions(vision, {
+baseOptions: {
+modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+delegate: "GPU"
+},
+runningMode: runningMode,
+numHands: 2
+});
+demosSection.classList.remove("invisible");
+};
